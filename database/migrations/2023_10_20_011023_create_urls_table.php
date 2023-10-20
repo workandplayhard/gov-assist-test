9b4f893495e003bff8a608bb3e6b6465d0dad400
workandplayhard <gabryalzin@gmail.com>
2023-10-19 21:22:21 -0400

    added url model, migration and resource controller

---
+++ database/migrations/2023_10_20_011023_create_urls_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 2048);
+            $table->char('slug', 5)->nullable()->index();
+            $table->integer('views')->default(0)->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
